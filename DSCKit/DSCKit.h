83c070b255c9093665d9967e02015f52ac48b0d1
Shota Shimazu <hornet.live.mf@gmail.com>
2021-12-19 23:49:32 +0900

    Initial Commit

---
+++ DSCKit/DSCKit.h
@@ -0,0 +1,18 @@
+//
+//  DSCKit.h
+//  DSCKit
+//
+//  Created by Shota Shimazu on 2021/12/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DSCKit.
+FOUNDATION_EXPORT double DSCKitVersionNumber;
+
+//! Project version string for DSCKit.
+FOUNDATION_EXPORT const unsigned char DSCKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DSCKit/PublicHeader.h>
+
+
