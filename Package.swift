adfb1773416eff6ed3324a6f13537a88127eb630
Shota Shimazu <hornet.live.mf@gmail.com>
2022-04-06 22:47:01 +0900

    fix package error

--- Package.swift
+++ Package.swift
@@ -1,10 +1,17 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "DSCKit",
+    platforms: [
+        .macCatalyst(.v13),
+        .macOS(.v11),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v14)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,13 +25,22 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
+        .systemLibrary(
+            name: "CLevelDB",
+            providers: [
+                .brew(["leveldb"])
+            ]
+        ),
         .target(
             name: "DSCKit",
             dependencies: []),
+        
+        /*
         .target(
             name: "DSCLib",
-            publicHeadersPath: "include"
-        ),
+            // publicHeadersPath: "include"
+            dependencies: []),
+        */
         .testTarget(
             name: "DSCKitTests",
             dependencies: ["DSCKit"]),
